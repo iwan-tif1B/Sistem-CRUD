17df1e2aeb65930d55a7c7d066f770955fb239dd
iwan-tif1B <80775010+iwan-tif1B@users.noreply.github.com>
2021-10-10 07:28:39 -0700

    Add files via upload

---
+++ crud_iwan/home.php
@@ -0,0 +1,5 @@
+<center><br><br><br><br><br><br><br><br>
+<img src="assets/images/TIFF.png" width="200px" height="200px" /> <br>
+<font Size="6" face="Helvetica">Sistem Informasi Akademik</font> <br>
+<font Size="6">Teknik Informatika Politeknik Kampar</font>
+</center>
